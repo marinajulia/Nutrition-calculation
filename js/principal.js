1c717e757ed233e7f5f3e5e81a7e05028bfc94ff
marinajulia <marinajulia707@gmail.com>
2022-02-04 21:59:01 -0300

    feat: appendChild

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,36 @@ var titulo = document.querySelector(".titulo");
 var botao = document.querySelector("#adicionar-paciente");
 
 botao.addEventListener("click", function(){
-    console.log("Eu fui clicado");
+    event.preventDefault();
+    var form = document.querySelector("#form-adicionar");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var pesoTd = document.createElement("td");
+    var nomeTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
 });
 
 var pacientes = document.querySelectorAll(".paciente");
