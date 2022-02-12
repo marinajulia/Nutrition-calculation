ea947c6efc627106502b8ed82eb40c8eee016a69
marinajulia <marinajulia707@gmail.com>
2022-02-12 13:24:54 -0300

    refactor: remove patient

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,14 @@
 var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        this.remove();
-    });
-});
+tabela.addEventListener("dblclick", function(){
+    event.target.parentNode.remove();
+})
+
+
+// funciona, mas não para os novos cadastros pq não estavam na página quando foi carregado:
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick", function(){
+//         this.remove();
+//     });
+// });
