4e675aad1708950cf1299f263f02054757624c55
marinajulia <marinajulia707@gmail.com>
2022-02-12 14:50:51 -0300

    chore: add AJAX

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,14 +1,23 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
-botaoAdicionar.addEventListener("click", function(){
+botaoAdicionar.addEventListener("click", function () {
     var xrh = new XMLHttpRequest();
     xrh.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-    xrh.addEventListener("load", function(){
-        var resposta = xrh.responseText;
-        var pacientes = JSON.parse(resposta);
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+    xrh.addEventListener("load", function () {
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if (xrh.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xrh.responseText;
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status)
+            console.log(xhr.responseText)
+            erroAjax.classList.remove();
+        }
     })
     xrh.send();
 })
