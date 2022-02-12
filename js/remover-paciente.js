087571d93101bd743838c86bf32d8c0978d3c64c
marinajulia <marinajulia707@gmail.com>
2022-02-12 13:38:38 -0300

    chore: add animation in remove patient

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,12 +1,15 @@
-var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(){
-    event.target.parentNode.remove();
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500)
 })
 
 
 // funciona, mas não para os novos cadastros pq não estavam na página quando foi carregado:
+// var pacientes = document.querySelectorAll(".paciente");
 // pacientes.forEach(function(paciente){
 //     paciente.addEventListener("dblclick", function(){
 //         this.remove();
